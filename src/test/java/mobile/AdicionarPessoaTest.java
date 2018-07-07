d7cad75fdc5a377eaf217ec74a7ff5a8d462b7c2
Davi Hoffmann <davi.phoffmann@gmail.com>
2018-07-06 21:34:06 -0300

    .

--- src/test/java/mobile/AdicionarPessoaTest.java
+++ src/test/java/mobile/AdicionarPessoaTest.java
@@ -5,10 +5,34 @@
  */
 package mobile;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
 /**
  *
  * @author suporte
  */
 public class AdicionarPessoaTest {
+    
+    @Test
+    public void adicionarPessoaComSucesso() throws MalformedURLException{
+        File app = new File("C:/Users/suporte/Documents/pos/projeto-automacao-unidavi/app/pessoas.apk");
+        
+        DesiredCapabilities capacidade = new DesiredCapabilities();
+        capacidade.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        
+        AppiumDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capacidade);
+        
+        
+    }
     
 }
